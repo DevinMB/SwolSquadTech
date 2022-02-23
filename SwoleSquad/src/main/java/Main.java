e24487b728189fb0d5e4d280d555c84721111b7d
rangerBUTTS <63884066+rangerBUTTS@users.noreply.github.com>
2022-02-23 15:14:54 -0500

    okay actually have something now

--- SwoleSquad/src/main/java/Main.java
+++ SwoleSquad/src/main/java/Main.java
@@ -1,7 +1,15 @@
+//TODO: WOOOOOOO!!!!
+
+
+
 
 
 public class Main {
 
+
+    //TODO: NEED TO CALL DAO 'N STUFF
+
+    //MAIN
     public static void main(String[] args){
         System.out.println("heyoooo");
 
