b416f4d6f96affded987d24d8d0ab8e9d03b94da
rangerBUTTS <63884066+rangerBUTTS@users.noreply.github.com>
2022-02-23 20:51:00 -0500

    worrk

--- SwoleSquad/src/main/java/dao/dao_googleCal.java
+++ SwoleSquad/src/main/java/dao/dao_googleCal.java
@@ -1,11 +1,13 @@
 package dao;
-
+////TODO:https://howtodoinjava.com/gson/gson-parse-json-array/
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import models.Event;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
+
 import java.io.IOException;
 import java.lang.reflect.Array;
 import java.lang.reflect.Type;
@@ -15,10 +17,6 @@ import java.util.Objects;
 import javax.sql.DataSource;
 
 public class dao_googleCal {
-
-
-
-
 
 
     public static List<Event> getEvents() throws IOException {
@@ -31,30 +29,26 @@ public class dao_googleCal {
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         Request request = new Request.Builder()
-                .url("https://www.googleapis.com/calendar/v3/users/me/calendarList")
+                .url("https://www.googleapis.com/calendar/v3/calendars/devinmbutts@gmail.com/events")
                 .method("GET", null)
-                .addHeader("Authorization", "Bearer ya29.A0ARrdaM_cGkP4d6B2fQ80El6Y--XiG9cWKQps2rR1HwBTnEJUhKMN3Yw9Ix683rRRDGLCS0uO6FHcnHFsqOu-8T0AEsIfbXPj8TzNZlgyt8b3SOeV5ZZKtoabb8FimB0X-cixbyb-dRLwjrSbgCl2-LKjy6HR")
+                .addHeader("Authorization", "Bearer ya29.A0ARrdaM8YENnx2VdrBoMa1VI6hgYwzwtXbl7DP6rFU6mCxkrGyJ_VUMHdJ88C-jcwoKR86mU8_QQixfkPSmoJRL521J8cdHgtSNIR7Ye6ypZcFmzSRhuF70Cy8cblOo2xUmXhnNbr8riWFsZLRwuhtkwkDfNO")
                 .build();
-        //I think this is a JSON object below??
         Response response = client.newCall(request).execute();
+        //I think this is a JSON object below??
+
+        System.out.println(response);
+
 
         Gson gson = new Gson();
-//TODO:https://howtodoinjava.com/gson/gson-parse-json-array/
-//
-//        Type eventListType = new TypeToken<ArrayList<Event>>(){}.getType();
+        if(response.isSuccessful()) {
+            ResponseBody responseBody = client.newCall(request).execute().body();
+            Event entity = gson.fromJson(responseBody.string(), Event.class);
+            System.out.println(entity.getSummary());
+        }
+        //TODO: https://stackoverflow.com/questions/23070298/get-nested-json-object-with-gson-using-retrofit
+        //TODO: Convert JSON to List of Events (Define Event in Models Folder)
 
-        List<Event> events = gson.fromJson(response.toString(), );
-
-       //new Gson().fromJson(String.valueOf(response),Event.class);
-
-
-
-        System.out.println(response.body().string());
-//TODO: Convert JSON to List of Events (Define Event in Models Folder)
-
-
-
-        return events;
+        return null;
     }
 
 
